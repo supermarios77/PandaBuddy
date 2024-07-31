f85170efa4a39146ff29629d997c2a300a13cf0f
Mariya B <supermariyabros@gmail.com>
2024-07-31 16:31:16 +0300

    ✨ Add Header

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+
+import Navbar from "@/components/Navbar";
+
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "Panda Pal - Your friendly AI study companion.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+        <body>
+          <Navbar />
+          {children}
+        </body>
+    </html>
+  );
+}
