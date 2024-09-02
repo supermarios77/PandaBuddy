f04c6d93d3f44b43545e0fceb7d7736948346583
Mariya B <supermariyabros@gmail.com>
2024-09-02 20:12:55 +0300

    ✨ Add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,19 @@ import NotificationHandler from "@/components/NotificationHandler";
 import PrelineScript from "@/components/PrelineScript";
 
 export const metadata: Metadata = {
-  title: "Panda Buddy",
-  description: "Panda Buddy - Your friendly AI study companion.",
+  title: "Panda Buddy - AI-Powered Learning Companion",
+  description: "Panda Buddy is an AI-powered learning platform designed to enhance your study experience with interactive quizzes, gamification, and adaptive learning paths. Features include a Pomodoro timer, notes section, creative workspace, and more.",
+  keywords: "Panda Buddy, AI learning, interactive quizzes, gamification, study companion, educational app, Pomodoro timer, notes section, creative workspace",
+  author: "Your Name or Company",
+  openGraph: {
+    title: "Panda Buddy - AI-Powered Learning Companion",
+    description: "Panda Buddy is an AI-powered learning platform designed to enhance your study experience with interactive quizzes, gamification, and adaptive learning paths.",
+    url: "panda-buddy.vercel.app",
+    // @ts-ignore
+    image: "https://panda-buddy.vercel.app/site-preview.png",
+    siteName: "Panda Buddy",
+    type: "website",
+  }
 };
 
 export default function RootLayout({
